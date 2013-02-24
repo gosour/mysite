665be240a0b27900a13203ccd36798f44c7af3a0
Sourav Goswami <gosour@gmail.com>
2013-02-25 03:45:46 +0530

    pictures! YAY

--- js/myjs.js
+++ js/myjs.js
@@ -10,6 +10,7 @@
           '<h2>But when done it'+"'s" + ' gonna be legen..</h2>' +
           '</div>'
       var d = document.getElementById("home");
+      document.getElementById("extra").innerHTML = '<span><img src="/images/nowwhat.png"></span><span class="pull-right"><img src="/images/nowwhat2.png"></span>'
       d.className = d.className + " active";
       document.getElementById("rants").className = "";
       document.getElementById("about").className = "";
@@ -24,6 +25,7 @@
             'You can also follow my novice code in my <a href="https://www.github.com/gosour">github profile</a>.' +
           '</h4>' +
           '</div>' 
+      document.getElementById("extra").innerHTML = '<span style="padding:30%"><img src="/images/rant.png"></span>'
       var d = document.getElementById("rants");
       d.className = d.className + " active";
       document.getElementById("home").className = "";
@@ -32,12 +34,14 @@
 
    function showAbout(){
       document.getElementById("text").innerHTML = ' <div>' +
-          '<img src="/images/me.jpg" class="img-circle">' +
-          '<div class="pull-right" class="well">' +
-          '<!-- Place this code where you want the badge to render. -->' +
-          '<a href="//plus.google.com/112383595984506575124?prsrc=3" rel="publisher" style="text-decoration:none;">' +
-          '<img src="//ssl.gstatic.com/images/icons/gplus-64.png" alt="Google+" style="border:0;width:64px;height:64px;"/></a>' +
+          '<span><a = href="/" class="pull-left"> <img src="/images/me.jpg" class="img-circle"></a></span>' +
+          '<span style="padding-left:20%;"><a = href="https://plus.google.com/112383595984506575124"><img src="/images/plus.jpg" class="img-circle"></a></span>' +
+          '<span><a = href="https://github.com/gosour" class="pull-right"><img src="/images/octo.png" class="img-circle"></a></span>' +
           '</div>'
+
+      document.getElementById("extra").innerHTML = '<ul>' +
+      '<li><h5>Not yet optimized for small screens</h5></li>' +
+      '<li><h5>Previous stuff now <a href="http://hello.souravgoswami.com">here</a></li>' 
 
       var d = document.getElementById("about");
       d.className = d.className + " active";
