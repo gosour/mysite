fad3d8cad62cf06bc1a66c0c7733bc0a0555900c
Sourav Goswami <gosour@gmail.com>
2013-03-06 01:24:34 +0530

    more readable

--- mySite.py
+++ mySite.py
@@ -70,7 +70,7 @@ class Permlink(BaseHandler):
     def get(self,blog_id): #if parentheses in url matcher 
         s = Blog.get_by_id(int(blog_id))
         if not s:
-            self.redirect('/404')
+            self.write('/404')
             return
 
         self.render('blogpost.html',blog = s)
